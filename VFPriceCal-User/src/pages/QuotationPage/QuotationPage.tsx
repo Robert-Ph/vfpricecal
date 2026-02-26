d3343755e6973df810a1514008aa3af8bfc506f0
Robert-Ph <huuthinh19593@gmail.com>
2026-02-26 22:41:20 +0700

    Code UI Quotation Page

--- VFPriceCal-User/src/pages/QuotationPage/QuotationPage.tsx
+++ VFPriceCal-User/src/pages/QuotationPage/QuotationPage.tsx
@@ -1,10 +1,17 @@
-
+import "./quotationPage.scss";
 
 const QuotationPage = () => {
     return (
-        <div>
-            <h1>Quotation Page</h1>
-            <p>This is the quotation page.</p>
+        <div className="body">
+            <div className="info">
+                <div className="info-item info-item-1"></div>
+                <div className="info-item info-product"></div>
+                <div className="info-item info-quantity"></div>
+            </div>
+            <div className="quotation">
+                <p>This is the quotation page.</p>
+            </div>
+
         </div>
     );
 }
