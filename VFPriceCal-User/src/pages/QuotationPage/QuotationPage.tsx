a9c1e68c9351e34b075b9d391bb6e76fe8ebaa6d
Robert-Ph <huuthinh19593@gmail.com>
2026-02-27 22:53:31 +0700

    code Quotation page: form info

--- VFPriceCal-User/src/pages/QuotationPage/QuotationPage.tsx
+++ VFPriceCal-User/src/pages/QuotationPage/QuotationPage.tsx
@@ -4,7 +4,26 @@ const QuotationPage = () => {
     return (
         <div className="body">
             <div className="info">
-                <div className="info-item info-item-1"></div>
+                <div className="info-item info-item-1">
+                    <div className="item-lable">
+                        <h3>Thông tin chung</h3>
+                    </div>
+                    <div className="item-content">
+                        <div className="form-name">
+                            <label htmlFor="name">Tên báo giá:</label>
+                            <input type="text" id="name" name="name" />
+                        </div>
+
+                        <div className="form-type-customer">
+                            <label htmlFor="customer-type">Loại khách hàng:</label>
+                            <select id="customer-type" name="customer-type">
+                                <option value="individual">Cá nhân</option>
+                                <option value="company">Doanh nghiệp</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+
                 <div className="info-item info-product"></div>
                 <div className="info-item info-quantity"></div>
             </div>
