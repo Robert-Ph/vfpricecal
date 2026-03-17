52de5676566a7066d625c975cdac06cec14431e0
Robert-Ph <huuthinh19593@gmail.com>
2026-03-17 21:58:37 +0700

    create spring boot backend

---
+++ vfprint/src/main/java/vfprint/com/example/vfprint/VfprintApplication.java
@@ -0,0 +1,13 @@
+package vfprint.com.example.vfprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VfprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VfprintApplication.class, args);
+	}
+
+}
---
+++ vfprint/src/test/java/vfprint/com/example/vfprint/VfprintApplicationTests.java
@@ -0,0 +1,13 @@
+package vfprint.com.example.vfprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VfprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
